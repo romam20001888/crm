8bdbe21dca4b313ad4590bedba2c1289a8765fd6
roman20001888 <romanrogankov2012@gmail.com>
2021-03-02 16:16:21 +0300

    1

--- crm/index.php
+++ crm/index.php
@@ -1,1 +1,14 @@
 <?include $_SERVER['DOCUMENT_ROOT'].'/crm/header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=WebSiteSetting::GetTitle()?></title>
+    <link rel="stylesheet" href="/crm/lib/style.css">
+</head>
+<body>
+    
+</body>
+</html>
